b8fe7172e3c2d3846a21be0dac84ec30d90ef51a
ArturoDeveloper <carlitorrego123@gmail.com>
2023-09-11 19:58:54 -0500

    formation aplication and configuration environment finished

---
+++ formation/serializers/formation_area_serializer.py
@@ -0,0 +1,44 @@
+from formation.models import FormationArea, Program
+from rest_framework import serializers
+from formation.models import TypeProgram
+
+
+class FormationAreaSerializerList(serializers.ModelSerializer):
+    
+    class Meta:
+        model = FormationArea
+        fields = [
+            'id',
+            'title',
+            'description',
+            'status',
+            'image',
+
+        ]
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['link'] = f'areas/{instance.slug}/'
+        # Get info of models 
+        id = instance.id
+        model_program = Program.objects.filter(area = id)
+        model_type_program = TypeProgram.objects.all()
+        
+        # Get type_programs of model type_program
+        types_programs = [('type_programs', i.type_program) for i in model_type_program ]
+        
+        # Build the serializer 
+        data['type_programs'] = [
+                # Load the information with model and compress list 
+                {key: value, 
+                # Get programs and validate with respect to area
+                'programs': [
+                    {
+                        # Get atributres for program if belongs area and type_program
+                        'id': program.id,
+                        'name': program.program
+                    } for program in model_program if value.__eq__(program.type_program.type_program)
+                ]} for key, value in types_programs
+            ]
+        
+        # Return the data with serializer 
+        return data
---
+++ formation/views/formation_area_views.py
@@ -0,0 +1,11 @@
+from formation.serializers.formation_area_serializer import FormationAreaSerializerList
+from rest_framework import viewsets
+
+class FormationAreaViewSet(viewsets.ModelViewSet):
+    # Support Serializer 
+    serializer_class = FormationAreaSerializerList
+    lookup_field = 'slug'
+    lookup_url_kwarg = 'slug'
+    # Complement for administration of model ViewSet
+    def get_queryset(self):
+        return self.get_serializer().Meta.model.objects.filter(status = "active")
